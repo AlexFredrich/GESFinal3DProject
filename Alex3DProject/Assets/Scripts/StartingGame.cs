ca71433b3779d1ae1e60ffe2aa290fa7fba86a54
AlexFredrich <alexandra.fredrich@loop.colum.edu>
2017-12-11 11:34:51 -0600

    Creating a 3D Adventure game with a hint of a story and involving different scenes.

---
+++ Alex3DProject/Assets/Scripts/StartingGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartingGame : MonoBehaviour {
+
+    public void StartGameButtonClicked()
+    {
+        SceneManager.LoadScene("3DFirstScene");
+    }
+}
